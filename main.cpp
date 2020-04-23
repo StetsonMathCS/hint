1b11f688104615146d7db93933393aba2fbba493
Thuan Pham <thpham@stetson.edu>
2020-04-23 01:06:24 -0400

    New Change

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <string>
+#include <stats.h>
+#include <map>
 using namespace std;
 
 int main() {
-
-	int x = 9;
-	cout << "There are: " << x << " commands use";
+	map <string, int> myMap;
+	map["search"] = 0;
+	map["add"] = 0;
+	map["tag"] = 0;
+	map["answer"] = 0;
+	map["help"] = 0;
+	string user_input;
+	cin >> user_input;
+        if (user_input.compare("hint stats"){
+			
+	}else if (user_input.compare("hints search"){
+		myMap["search"] += 1
+	}	
+	
+	
+	
 	return 0;
 }
---
+++ stats.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class stats {
+	private:
+		int value;
+	public:
+		int 
+
+};
