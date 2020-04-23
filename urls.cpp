0b98fddffef1535c1c5b47ca0f30ee79ab1c20c3
Cole Spitzer <cspitzer@stetson.edu>
2020-04-22 23:14:26 -0400

    Trying to pip into JSON.hpp

--- urls.cpp
+++ urls.cpp
@@ -1,39 +1,75 @@
 #include <stdio.h>
 #include "curl/curl.h"
 #include "json.hpp"
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+using json = nlohmann::json;
+
+
+namespace
+{
+	size_t callback(
+			const char* in,
+			size_t size,
+			size-t num,
+			string* out)
+	{
+		const size_t totalBytes(size *num);
+		out->append(in, totalBytes);
+		return totalBytes;
+	}
+}
 
 int main()
 {
+	string question;
+	cout << "Please Enter your Question" << endl;
+	getline(cin, question);
 
-	CURL* curl = curl_easy_init();
-	if(!curl)
+	CURL *curl;
+	CURLcode result;
+	curl = curl_easy_init();
+
+	if(curl)
 	{
-		fprintf(stderr, "curl failure");
-		return 0;
+
+		//easy_curl_setopt is setting the options for my curl
+		curl_easy_setopt(curl, CURLOPT_URL, "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle={question}&site=stackoverflow&filter=!BHMIbze.x2vCH_S4H0ZisKe_lF0yCi");
+
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+
+		result = curl_easy_perform(curl);
+
+		//making sure that the curl is ok and worked right
+		if(result != CURLE_OK)
+		{
+			cout << curl_easy_strerror(result) << endl;
+			return 1;
+		}
+
+		//making that the website gave info 
+		int httpCode(0);
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);	
+		curl_easy_cleanup(curl);
+
+		//if website worked it will be equal to 200
+		if(httpCode == 200)
+		{
+			cout << "Sucessful Response From URL" << endl;
+
+			json jsonData;
+			json jsonReader;
+		}
+
+
+
 	}
-
-	CURLcode res = CURLE_OK;
-	if((res = curl_easy_setopt(curl, CURLOPT_URL, "https://stackoverflow.com/questions/11828270/how-do-i-exit-the-vim-editor/28342808#28342808")) != CURLE_OK)
-	{
-		fprintf(stderr, curl_easy_strerror(res));
-		return 1;
-	}
-	if((res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)) != CURLE_OK)
-	{
-		fprintf(stderr, curl_easy_strerror(res));
-		return 1;
-
-	}
-	if((res = curl_easy_perform(curl)) != CURLE_OK)
-	{
-		fprintf(stderr, curl_easy_strerror(res));
-		return 1;
-	}
-	
-
-
-
-
-	curl_easy_cleanup(curl);
 	return 0;
+
+
+
 }
