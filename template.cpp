4e52d85167cf3348dd38aaaba2673871874d3641
Joey Mathison <jmathison@stetson.edu>
2020-04-24 17:50:45 -0400

    i haven't tested this at all and it probably doesn't work but i need to commit SOMETHING just in case i fail to figure out what i'm doing before midnight

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+using namespace std;
+
+int main()
+{
+    string new_help_file_name, full_name_line;
+    char* pEnv;
+    pEnv = getenv("EDITOR");
+    
+    fs::create_directory("temp");
+    fs::copy("helptemplate.txt", "temp/newhelp.txt");
+
+    auto ftime = fs::last_write_time("temp/newhelp.txt");
+    auto ftime2 = fs:last_write_time("temp/newhelp.txt");
+
+    system("xdg-open newhelp.txt");
+
+    while(ftime == ftime2){
+	ftime2 = fs:last_write_time("temp/newhelp.txt");
+    }
+
+    ifstream tempf("temp/newhelp.txt");
+    if(tempf.is_open()){
+	for(int i = 1; i <= 2; ++i){
+	    getline(tempf, full_name_line);
+        }
+    }
+    tempf.close();
+
+    new_help_file_name = full_name_line.substr(6) + ".txt";
+
+    fs::copy("temp/newhelp.txt", new_help_file_name);
+    fs::remove_all("temp");
+}
