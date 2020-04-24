b26e50b95d44f96f2077b2e5631d9b866b71b94d
Thuan Pham <thpham@stetson.edu>
2020-04-24 04:09:20 -0400

    Getting times of using each command, printing out the time history of those using commands too

--- Makefile
+++
@@ -1,9 +0,0 @@
-output: main.o stats.o
-	g++ main.o stats.o -o output
-main.o: main.cpp
-	g++ -c main.cpp
-stats.o: stats.cpp stats.h
-	g++ -c stats.cpp
-
-clean: 
-	rm*.o output
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
-#include "stats.h"
+#include <vector>
+#include <map>
+#include <ctime>
+#include <time.h>
+
 #define ar array
 using namespace std;
 
@@ -13,17 +17,56 @@ string getCommand() {
 	return a[1];
 }
 
+void display(map<string, map<string, string> > *myMap, map<string, map<string, int> > *count, vector<string> *myvec){
+	map<string, map<string, string> >::const_iterator it;
+	map<string, string>::const_iterator another;
+	
+
+
+	for (it = myMap->begin(); it != myMap->end(); it++){
+		for(another = it->second.begin(); another != it->second.end(); another++){
+			cout << "Users " << it->first << " " << another->first << " " << count->at(it->first).at(another->first)  << " time(s)" << endl;
+		}
+	}
+}
+
 int main() {
+	map<string, map<string, string> > myMap;
+	map<string, map<string, int> > count;
+	vector<string> myvec;
+
 	while (true){
+		string another = "";
 		string user_input = getCommand();
-		if (user_input.compare("stats") != 0){
+		time_t thetime = time(0);
+		string mytime = asctime(localtime(&thetime));
+
+		if ((user_input.compare("stats") == 0)){
+			display(&myMap, &count, &myvec);	
+		}else if(user_input.compare("recent") == 0){
+			cout << "Recent command: " << endl;
+			cout << "'" << myvec.back() << "'" << endl;	
+		}else{
 			string textInput;
 			cout << user_input << ": ";
 			cin >> textInput;
-			Stats a(user_input, textInput);
-		
-		}else{
-			a.display();
+			myMap[user_input][textInput] = mytime;
+			another.append(user_input);
+			another.append(": ");
+			another.append(textInput);
+			another.append(" at ");
+			another.append(mytime);
+			myvec.push_back(another);	
+
+			if (myMap.count(user_input)){
+				if (myMap[user_input].count(textInput)){
+					count[user_input][textInput] += 1;
+				}else {
+					count[user_input][textInput] = 1;
+				}
+			}else{
+				count[user_input][textInput] = 1;
+			}
 		}
 	}
 	return 0;
