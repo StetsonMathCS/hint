1a39725d20de9ca872ea96e34bd41f31b16f683f
Cole Spitzer <cspitzer@stetson.edu>
2020-04-23 21:46:51 -0400

    Trying to hard code URL becasue it wotn work

--- urls.cpp
+++ urls.cpp
@@ -1,37 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "curl/curl.h"
-#include "json.hpp"
 #include <string>
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include "json/json.h"
+#include "json.hpp"
 using json = nlohmann::json;
+using namespace std;
+
+
+
+
+
 
 int main()
 {
 	//thsi is the input from the user
-	string question;
-	cout << "Please Enter your Question Using EXACTLY 2 Words" << endl;
-	getline(cin, question);
+	//	string worda, wordb;
+	//	cout << "Please Enter your Question Using EXACTLY 2 Words" << endl;
+	//	cin >> worda >> wordb;
+	//	string question = worda + "%20" + wordb;
+	//	cout << question;	
 
 	//initiating the curl
 	CURL *curl;
 	CURLcode result;
 	curl = curl_easy_init();
-	string url = "api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=" + question + "&site=stackoverflow&filter=!)8aCqWxCWk*)CTW";
 
+	FILE* file = fopen("urls.txt", "w");
 	if(curl)
 	{
-		curl_easy_setopt(curl, CURLOPT_URL, ("api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle={question}&site=stackoverflow&filter=!)8aCqWxCWk*)CTW"));
-	
+		curl_easy_setopt(curl, CURLOPT_URL, ("https://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle=close%20vim&site=stackoverflow&filter=!)8aCqWxCWk*)CTW"));
+
 		//follow URLS
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-		
+
 		//sets a waiting time before it times out waiting for a response form the website
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
 
 		//decodes weird charcters
 		curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip");
+
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
 
 		//actually getting the data
 		result = curl_easy_perform(curl);
@@ -51,7 +62,23 @@ int main()
 		//if website worked it will be equal to 200
 		if(httpCode == 200)
 		{
+			ifstream ifile("urls.txt");
+
 			cout << "Sucessful Response From URL" << endl;
+			string line;
+
+			if(ifile.is_open())
+			{
+				cout << "Attemting To Get Answer ID" << endl;
+				while(getline(ifile, line))
+				{
+					cout << "Turninng File into JSON" << endl;
+					stringstream s(line);
+					json j_done = json::parse(s);
+					
+					
+				}
+			}
 
 
 
