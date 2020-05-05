c26627077f38733cec8b19260362e87bf75cac91
Thuan Pham <thpham@stetson.edu>
2020-05-05 01:00:13 -0400

    Finishing up with the data structures using map<string, string>

--- datastructures.cpp
+++ datastructures.cpp
@@ -8,53 +8,72 @@ using namespace std;
 DataStructures::DataStructures(){
 }
 
-void DataStructures::add(const string word, const string def){
-	if (myMap.count(word)){
+void DataStructures::add(const string word, const string def){ 		//-Adding new word and its definition into the map 
+	if (myMap.count(word)){						//-Checking if that word is already existed
 		cout << word << " is already defined. Update? (Y/N)";
 		string input;
 		cin >> input;
 		if (input.compare("Y") == 0){
-			return DataStructures::update(word, def);
-		else {
-			break;
-		     }
+			return DataStructures::update(word, def);	//-If yes going to the update function
+		}else{
+			return;
 		}	
 	}else {
-		myMap[word] = def;
+		myMap[word] = def;		//-If no adding the new word
+		wordPos[word] = index;		//-Keeping track with the sequence of the map
+		index+=1;
 	}
-	myvec.push_back(myMap);
+
 }
 
 
 void DataStructures::update(const string word, const string def){
-	myMap[word] = def;
-	for (int i = 0; i < myvec.size(); i++){
-		if (myvec[i].compare(word) == 0){
-			myvec[i][word] = def;
+	if (myMap.count(word)){			// -Update the existed word
+		myMap[word] = def;
+	}else{					//-If that word is not exist -> creating a new one by using add function
+		return DataStructures::add(word, def);
+	}
+
+}
+
+void DataStructures::display() {	//-Print out all the element of the map, word
+	map<string, string>::const_iterator it;
+	for (it = myMap.begin(); it != myMap.end() ; it++){
+		cout << wordPos[it->first] << ": " << it->first << ": " << it->second << endl;
+	}
+}
+
+void DataStructures::rm(const int index){	//-Removing one of the word of the map
+	map<string, string>::const_iterator it;	//-Iterate through the map in order to get the string
+						
+	for (it = myMap.begin(); it != myMap.end(); it++){
+		if (wordPos[it->first] == index){
+			myMap.erase(it->first);
+			wordPos.erase(it->first);
+		}
+
+	}
+}
+
+string DataStructures::recent() const{		//-Function return the lastest word that put into the map
+	map<string, string>::const_iterator it = myMap.end();
+	string result = "";
+	result.append(it->first);
+	result.append(": ");
+	result.append(it->second);
+	return result;
+}
+
+string DataStructures::search(const string word) const{		//-Function searching through the map find the definition, return the def of that word
+	map<string, string>::const_iterator it;
+	string result = "";	//-Empty string to add to make a sentence
+	for (it = myMap.begin(); it != myMap.end(); it++){
+		if (it->first.compare(word) == 0){
+			result.append(it->first);
+			result.append(": ");
+			result.append(it->second);
+			return result;
 		}
 	}
-}
-
-void DataStructures::display() {
-	int num = 0;
-	map<string, string>::iterator it;
-	for (it = myMap.begin(); it != myMap.end() ; it++){
-		cout << num << ". " << it->first << ": " << it->second << endl;
-		num++;
-	}
-}
-
-void DataStructures::rm(const int index){
-	vector<string> another;
-	map<string, string>::iterator it;
-	myvec.erase(myvec.at(index-1);
-
-	for (it = myMap.begin(); it != myMap.end(); it++){
-		another.push_back(it->first);
-	}	
-	mymap.erase(myvec.at(index-1));
-}
-
-string DataStructures::recent() const{
-	return "a";
+	return "Can't find any word";	//-return this statement if didn't not find that word
 }
--- datastructures.h
+++ datastructures.h
@@ -6,15 +6,17 @@ using namespace std;
 
 class DataStructures {
 	private:
-		string word;
-		string def;
+		string word;	//-Word input
+		string def;	//-Word def input
+		int index = 1;	//-Sequence
 	public:
 		DataStructures();
 		map<string, string> myMap;
-		vector<map<string, string> > myvec;
+		map<string, int> wordPos;
 		void add(const string word, const string def);
 		void rm(const int index);
 		void display();
 		void update(const string word, const string def);
 		string recent() const;
+		string search(const string word) const;
 };
